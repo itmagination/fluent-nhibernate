821de1e67907b16898a1cfa6d5338f1bf1181578
ITMAGINATION <github@itmagination.pl>
2015-09-25 14:12:24 +0200

    Added "albacore" to InstallGems script.

--- InstallGems.bat
+++ InstallGems.bat
@@ -5,5 +5,9 @@ echo Setting up rake environment for building
 echo Installing Bundler
 gem install bundler
 
+echo Installing albacore
+gem install albacore --version "<=1.0"
+
 echo Bundle Installing gems
 bundle install
+
